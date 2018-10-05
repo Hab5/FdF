6d71ccca347b82bde463e5f582de8925791efdc4
Max Bellaiche <toor@hab.local>
2018-10-05 22:25:51 +0200

    time to eat

--- srcs/map_size.c
+++ srcs/map_size.c
@@ -10,7 +10,7 @@ char *get_line(char *map)
     char *line;
 
     fd = open(map, O_RDONLY);
-    while(get_next_line(fd, &str)!= 0)
+    while(get_next_line(fd, &str) != 0)
     {
         line = str;
     }
@@ -48,13 +48,4 @@ int count_columns(char *line)
     if (line[ft_strlen(line) - 1] != ' ')
         return(space + 1);
     return (space);
-
 }
-
-/*int main(int argc, char **argv)
-{
-    printf("len: %ld\n", ft_strlen(get_line(argv[1])));
-    printf("Rows : %d\n", count_rows(argv[1]));
-    printf("Columns : %d\n", count_columns(get_line(argv[1]))); 
-    return (0);
-}*/
